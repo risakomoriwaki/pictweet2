4bf01eaf919154e5ace0f98ad56a8286d47a3e52
moriwakirisako <risakomoriwaki@gmail.com>
2019-11-19 21:52:49 +0900

    削除機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get   'tweets'      =>  'tweets#index'     #ツイート一覧画面
   get   'tweets/new'  =>  'tweets#new'       #ツイート投稿画面
   post  'tweets'      =>  'tweets#create'    #ツイート投稿機能
+  delete  'tweets/:id'  => 'tweets#destroy'
   get   'users/:id'   =>  'users#show'    #Mypageへのルーティング
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_18_122748) do
+ActiveRecord::Schema.define(version: 2019_11_19_113310) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
     t.text "text"
     t.text "image"
     t.datetime "created_at"
